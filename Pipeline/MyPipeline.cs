edf3951bc0f8421293991d1c88651e84fa805566
wael101 <wael.musleh@gmail.com>
2019-04-23 17:48:24 -0700

    Check-In 1st time - base project

---
+++ Pipeline/MyPipeline.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using RazorCart.Core.ActionPipeline;
+
+namespace MyCompany.RazorCart.Integration
+{
+    public class MyPipeline : PipelineManager
+    {
+        /// <summary>
+        /// Register the Pipeline Action List to be executed once an item(s) requested to be added to the cart
+        /// </summary>
+        /// <returns>A <see cref="RazorCart.Core.ActionPipeline.PipelineAction[]"/></returns>
+        protected override PipelineAction[] ListCartPipelineActions()
+        {
+            return base.ListCartPipelineActions();
+        }
+
+        /// <summary>
+        /// Register the Pipeline Action List to be executed on cart totals calculation request
+        /// </summary>
+        /// <returns>A <see cref="RazorCart.Core.ActionPipeline.PipelineAction[]"/></returns>
+        protected override PipelineAction[] ListCalculatorPipelineActions()
+        {
+            return base.ListCalculatorPipelineActions();
+        }
+
+        /// <summary>
+        /// Register the Pipeline Action List to be executed on cart checkout/place order
+        /// </summary>
+        /// <returns>A <see cref="RazorCart.Core.ActionPipeline.PipelineAction[]"/></returns>
+        protected override PipelineAction[] ListCheckoutPipelineActions()
+        {
+            return base.ListCheckoutPipelineActions();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# MyCompany.RazorCart.Integration
+# Project Description
+
+This an [Action Delegate Pipeline](https://helpdesk.smith-consulting.com/kb/a1454/action-delegate-pipeline.aspx) example project for [RazorCart](https://www.smith-consulting.com/Products/Shopping-Cart)
